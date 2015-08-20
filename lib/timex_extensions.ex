73af8b73e16f4acf26df725afa47237e42f83e25
Kevin C. Baird <kcbaird@world.oberlin.edu>
2015-08-20 11:56:57 -0400

    Pattern matching for nil case

--- lib/timex_extensions.ex
+++ lib/timex_extensions.ex
@@ -18,49 +18,46 @@ defmodule OpenAperture.Timex.Extensions do
   String containing the timestamp
   """
   @spec get_elapsed_timestamp(term) :: String.t
+  def get_elapsed_timestamp(nil), do: "<unknown>"
   def get_elapsed_timestamp(start_time) do
-    if (start_time == nil) do
-      "<unknown>"
+    timestamp_secs = Time.elapsed(start_time, :secs)
+    if (timestamp_secs < 0) do #time will be negative if in the future
+      timestamp_secs = timestamp_secs * -1
+    end
+
+    if (timestamp_secs < 60) do
+      "#{round timestamp_secs} second(s)"
     else
-      timestamp_secs = Time.elapsed(start_time, :secs)
-      if (timestamp_secs < 0) do #time will be negative if in the future
-        timestamp_secs = timestamp_secs * -1
+      timestamp_mins = Time.elapsed(start_time, :mins)
+      if (timestamp_mins < 0) do #time will be negative if in the future
+        timestamp_mins = timestamp_mins * -1
       end
 
-      if (timestamp_secs < 60) do
-        "#{round timestamp_secs} second(s)"
+      if (timestamp_mins < 60) do
+        "#{round timestamp_mins} minute(s)"
       else
-        timestamp_mins = Time.elapsed(start_time, :mins)
-        if (timestamp_mins < 0) do #time will be negative if in the future
-          timestamp_mins = timestamp_mins * -1
+        timestamp_hours = Time.elapsed(start_time, :hours)
+        if (timestamp_hours < 0) do #time will be negative if in the future
+          timestamp_hours = timestamp_hours * -1
         end
 
-        if (timestamp_mins < 60) do
-          "#{round timestamp_mins} minute(s)"
+        if (timestamp_hours < 24) do
+          "#{round timestamp_hours} hour(s)"
         else
-          timestamp_hours = Time.elapsed(start_time, :hours)
-          if (timestamp_hours < 0) do #time will be negative if in the future
-            timestamp_hours = timestamp_hours * -1
+          timestamp_days = Time.elapsed(start_time, :days)
+          if (timestamp_days < 0) do #time will be negative if in the future
+            timestamp_days = timestamp_days * -1
           end
 
-          if (timestamp_hours < 24) do
-            "#{round timestamp_hours} hour(s)"
+          if (timestamp_days < 7) do
+            "#{round timestamp_days} day(s)"
           else
-            timestamp_days = Time.elapsed(start_time, :days)
-            if (timestamp_days < 0) do #time will be negative if in the future
-              timestamp_days = timestamp_days * -1
+            timestamp_weeks = Time.elapsed(start_time, :weeks)
+            if (timestamp_weeks < 0) do #time will be negative if in the future
+              timestamp_weeks = timestamp_weeks * -1
             end
 
-            if (timestamp_days < 7) do
-              "#{round timestamp_days} day(s)"
-            else
-              timestamp_weeks = Time.elapsed(start_time, :weeks)
-              if (timestamp_weeks < 0) do #time will be negative if in the future
-                timestamp_weeks = timestamp_weeks * -1
-              end
-
-              "#{round timestamp_weeks} week(s)"
-            end
+            "#{round timestamp_weeks} week(s)"
           end
         end
       end
